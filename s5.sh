b07bf252bd55cd5207e3f2a5e05bbe01ee4499b7
Nicole Sani <nixyutube@gmail.com>
2026-06-08 23:46:29 +0000

    Semana 5

---
+++ s5.sh
@@ -0,0 +1,18 @@
+# Comando para limpiar nuestra terminal
+clear
+# Comando utilizado para tener lo permisos de super usuario y conectarse
+# a los servidores de los repositorios y descarga la lista más reciente de software disponible
+sudo apt update
+# Comando utilizado para descargar e instalar las versiones más 
+# recientes de todos los programas y librerías que ya tienes instalados en tu máquina
+sudo apt upgrade
+#Le indica al gestor de paquetes que busque, descargue e instale específicamente Nmap
+sudo apt install nmap
+# Crear un archivo
+touch suma.py
+# Abre el archivo en el editor de texto Nano para escribir el código
+nano suma.py
+# Muestra el contenido guardado en el archivo directamente en la terminal
+cat suma.py
+# Ejecuta el script utilizando el interprete de Python
+python calculadora.py
