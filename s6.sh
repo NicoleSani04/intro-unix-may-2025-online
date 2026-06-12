801a7b103498900adc63c0f0452f4694d1c2ddb5
Nicole Sani <nixyutube@gmail.com>
2026-06-12 00:50:08 +0000

    wip: ping with arguments

---
+++ s6.sh
@@ -0,0 +1,14 @@
+# Comando para buscar los archivos que contengan los caracteres específicados
+ls | grep "bash"
+#El comando le dice al sistema operativo que utilice el intérprete bash para 
+#leer y ejecutar las instrucciones contenidas en el archivo
+bash /workspaces/intro-unix-may-2025-online/ping_with_arguments.sh
+# Comando para descargar una librería
+sudo apt install iputils-ping
+# Comando para hacer ping a nuestra tarjeta de red
+bash /workspaces/intro-unix-may-2025-online/ping_with_arguments.sh 127.0.0.1
+# Pausa cualquier proceso que se esté ejecutando
+ctrl + c
+
+# Se muestran todas las variables de entorno del sistema operativo
+env
